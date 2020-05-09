ae7a2fd1cd2ecae307dd53d1354cc8a23969bed7
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-05-10 00:16:25 +0200

    Delete comments, rename to be me precise, add a strip() ...

--- jankenpon/app.py
+++ jankenpon/app.py
@@ -14,34 +14,35 @@ from jankenpon.player_classes import (Player, HumanPlayer,
                                       ReflectPlayer, RandomPlayer, MOVES)
 from jankenpon.tools import hinder_invalid_input, beats
 
+OPPONENTS = [BadLuckPlayer(), InteligentReflectPlayer(),
+             ReflectPlayer(), CyclePlayer(),
+             RandomPlayer(), RockPlayer()]
 
 class Game:
     def __init__(self, p1, p2):
         print(INTRO)
-        self.point_P1 = 0
-        self.point_P2 = 0
-        self.my_name = ''
-        self.computer_name = ''
-        self.opon = 0
-        self.oponents = [BadLuckPlayer(), InteligentReflectPlayer(),
-                         ReflectPlayer(), CyclePlayer(),
-                         RandomPlayer(), RockPlayer()]
-        self.revealStrategyMode = False
+        self.p1 = p1
+        self.p2 = p2
+        self.score_p1 = 0
+        self.score_p2 = 0
+        self.opponent_name_index = 0
+        self.reveal_strategy_mode = False
         self.chooseGame()
 
     def chooseGame(self):
-        self.game_option = hinder_invalid_input(MENU_OPTIONS_PROMPT , MENU_OPTIONS)
+        self.game_option = hinder_invalid_input(MENU_OPTIONS_PROMPT , 
+                                                MENU_OPTIONS)
 
         if self.game_option == '1':
             self.p1 = HumanPlayer()
-            self.p2 = random.choice(self.oponents)
+            self.p2 = random.choice(OPPONENTS)
             self.get_player_name()
             self.get_computer_name()
             self.game_hum_vs_comp()
 
         elif self.game_option == '2':
-            self.p1 = random.choice(self.oponents)
-            self.p2 = random.choice(self.oponents)
+            self.p1 = random.choice(OPPONENTS)
+            self.p2 = random.choice(OPPONENTS)
             self.game_comp_vs_comp()
 
         elif self.game_option == '3':
@@ -57,16 +58,16 @@ class Game:
 
     def get_player_name(self):
         print('What is your name?')
-        self.my_name = input()
-        if self.my_name == '':
-            self.my_name = 'Player 1'
+        self.p1.set_name(input().strip())
+        if self.p1.name == '':
+            self.p1.set_name('Player 1')
 
     def get_computer_name(self):
-        if self.opon == len(OPPONENT_NAMES) - 1:
-            self.opon = 0
+        if self.opponent_name_index == len(OPPONENT_NAMES) - 1:
+            self.opponent_name_index = 0
         else:
-            self.opon += 1
-        self.computer_name = OPPONENT_NAMES[self.opon]
+            self.opponent_name_index += 1
+        self.p2.set_name(OPPONENT_NAMES[self.opponent_name_index])
 
     def play_game_hum(self):
         print("\n     Game start!\n")
@@ -88,21 +89,21 @@ class Game:
             print('Do you want to know your opponent\'s strategy? (yes or no)')
             reveal_mode = hinder_invalid_input(SORRY_YES_NO, 'yes no')
             if reveal_mode == 'yes':
-                self.revealStrategyMode = True
+                self.reveal_strategy_mode = True
             else:
-                self.revealStrategyMode = False
+                self.reveal_strategy_mode = False
             self.get_computer_name()
-            self.p2 = random.choice(self.oponents)
+            self.p2 = random.choice(OPPONENTS)
             self.game_hum_vs_comp()
         else:
             self.__init__(Player(), Player())
 
     def play_game_com(self):
         print("\n   Game start!")
-        self.my_name = random.choice(OPPONENT_NAMES)
-        self.computer_name = random.choice(OPPONENT_NAMES)
-        if self.my_name == self.computer_name:
-            self.computer_name = self.my_name + ' Bad Twin'
+        self.p1.set_name(random.choice(OPPONENT_NAMES))
+        self.p2.set_name(random.choice(OPPONENT_NAMES))
+        if self.p1.get_name() == self.p2.get_name():
+            self.p2.set_name(self.p1.get_name() + ' Bad Twin')
         for round in range(3):
             print('\n\x1b[6;37;41m' + f" X---- ROUND {round+1} ----X "
                   + '\x1b[0m\n')
@@ -116,8 +117,8 @@ class Game:
               'Do you want to watch another match? (yes or no)')
         otherMatch = hinder_invalid_input(SORRY_YES_NO, 'yes no')
         if otherMatch == 'yes':
-            self.p1 = random.choice(self.oponents)
-            self.p2 = random.choice(self.oponents)
+            self.p1 = random.choice(OPPONENTS)
+            self.p2 = random.choice(OPPONENTS)
             self.game_comp_vs_comp()
         else:
             self.__init__(Player(), Player())
@@ -142,40 +143,40 @@ class Game:
             image2 = HANDS[2]
         strategy1 = self.p1.tactic()
         strategy2 = self.p2.tactic()
-        if strategy1 == '' and self.revealStrategyMode is False:
-            print('\x1b[31m' + f"{self.my_name}" + '\x1b[0m' +
+        if strategy1 == '' and self.reveal_strategy_mode is False:
+            print('\x1b[31m' + f"{self.p1.get_name()}" + '\x1b[0m' +
                   f": {move1} {image1}" +
-                  '\x1b[34m' + f"{self.computer_name}" + '\x1b[0m' +
+                  '\x1b[34m' + f"{self.p2.get_name()}" + '\x1b[0m' +
                   f": {move2} {image2}")
-        elif strategy1 == '' and self.revealStrategyMode is True:
-            print('\x1b[31m' + f"{self.my_name}" + '\x1b[0m' +
+        elif strategy1 == '' and self.reveal_strategy_mode is True:
+            print('\x1b[31m' + f"{self.p1.get_name()}" + '\x1b[0m' +
                   f" {strategy1}: {move1} {image1}" +
-                  '\x1b[34m' + f"{self.computer_name}" + '\x1b[0m' +
+                  '\x1b[34m' + f"{self.p2.get_name()}" + '\x1b[0m' +
                   f" {strategy2}: {move2} {image2}")
         else:
-            print('\x1b[31m' + f"{self.my_name}" + '\x1b[0m' +
+            print('\x1b[31m' + f"{self.p1.get_name()}" + '\x1b[0m' +
                   f" {strategy1}: {move1} {image1}" +
-                  '\x1b[34m' + f"{self.computer_name}" + '\x1b[0m' +
+                  '\x1b[34m' + f"{self.p2.get_name()}" + '\x1b[0m' +
                   f" {strategy2}: {move2} {image2}")
         if beats(move1, move2) is True:
-            self.point_P1 += 1
+            self.score_p1 += 1
         elif beats(move2, move1) is True:
-            self.point_P2 += 1
+            self.score_p2 += 1
         else:
             pass
-        print('\n\x1b[31m' + f"{self.my_name}" + '\x1b[0m' +
-              f": {self.point_P1} point(s) X " +
-              '\x1b[34m' + f"{self.computer_name}" + '\x1b[0m' +
-              f": {self.point_P2} point(s)")
+        print('\n\x1b[31m' + f"{self.p1.get_name()}" + '\x1b[0m' +
+              f": {self.score_p1} point(s) X " +
+              '\x1b[34m' + f"{self.p2.get_name()}" + '\x1b[0m' +
+              f": {self.score_p2} point(s)")
         self.p1.learn(move1, move2)
         self.p2.learn(move2, move1)
 
     def get_who_won(self):
-        if self.point_P1 > self.point_P2:
-            print('\n\x1b[31m' + f'{self.my_name} has won!' + '\x1b[0m')
-        if self.point_P1 < self.point_P2:
-            print('\n\x1b[34m' + f'{self.computer_name} has won!' + '\x1b[0m')
-        if self.point_P1 == self.point_P2:
+        if self.score_p1 > self.score_p2:
+            print('\n\x1b[31m' + f'{self.p1.get_name()} has won!' + '\x1b[0m')
+        if self.score_p1 < self.score_p2:
+            print('\n\x1b[34m' + f'{self.p2.get_name()} has won!' + '\x1b[0m')
+        if self.score_p1 == self.score_p2:
             print('\n\x1b[1m' + 'It is a tie!' + '\x1b[0m')
-        self.point_P1 = 0
-        self.point_P2 = 0
+        self.score_p1 = 0
+        self.score_p2 = 0
