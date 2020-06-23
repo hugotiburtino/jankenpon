ded6cb8d6b59731e40b0348dcf035847f8a8046e
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-06-23 22:45:49 +0200

    Remove from init functions and put them into new func intro
    Functions menu.show_intro, set_game and, from set_game, run_game
    have the same general idea and are more organized in a function
    for their own.
    Besides that, remove unused class and use isinstance instead of
    typeof, as it is more idiomatic. And fix format by black.

--- jankenpon/app.py
+++ jankenpon/app.py
@@ -7,17 +7,29 @@ and reports both player's scores each round as well as the possible winner
 
 import random
 import time
-from jankenpon.resources import (HANDS, SORRY_YES_NO)
-from jankenpon.player_classes import (Player, HumanPlayer,
-                                      RockPlayer, CyclePlayer,
-                                      BadLuckPlayer, InteligentReflectPlayer,
-                                      ReflectPlayer, RandomPlayer, MOVES)
+from jankenpon.resources import HANDS, SORRY_YES_NO
+from jankenpon.player_classes import (
+    HumanPlayer,
+    RockPlayer,
+    CyclePlayer,
+    BadLuckPlayer,
+    InteligentReflectPlayer,
+    ReflectPlayer,
+    RandomPlayer,
+    MOVES,
+)
 from jankenpon.tools import hinder_invalid_input, beats
 from jankenpon.menu import Menu
 
-OPPONENTS = [BadLuckPlayer, InteligentReflectPlayer,
-             ReflectPlayer, CyclePlayer,
-             RandomPlayer, RockPlayer]
+OPPONENTS = [
+    BadLuckPlayer,
+    InteligentReflectPlayer,
+    ReflectPlayer,
+    CyclePlayer,
+    RandomPlayer,
+    RockPlayer,
+]
+
 
 class Game:
     """
@@ -34,61 +46,65 @@ class Game:
 
     def __init__(self, p1, p2):
         """
-        magic function that starts the game setting players and values 
-        to default
+        Magic function that starts the game setting players and values 
+        to default.
         """
         self.p1 = p1
         self.p2 = p2
         self.score_p1 = 0
         self.score_p2 = 0
         self.reveal_strategy_mode = False
+    
+    def intro(self):
         self.menu.show_intro()
         self.set_game(self.menu.choose_game())
+        self.run_game()
 
     def set_game(self, game_type):
         """
-        Sets the the first play as human or computar, randomizes the second
-        player and runs the game
+        Sets the the first player as human or computar, randomizes the second
+        player and runs the game.
 
         ...
 
         Parameters
         ----------
         game_type : str
-            The type of game. 
+            The type of game.
             Options: human vs computer or computer vs computer
         """
 
-        if game_type == 'human_vs_computer':
+        if game_type == "human_vs_computer":
             self.p1 = HumanPlayer()
         else:
             self.p1 = random.choice(OPPONENTS)()
         self.p2 = random.choice(OPPONENTS)()
-        self.run_game()
 
     def run_game(self):
         """
-        Calls the stages of the game: play, get winner and display the game
-        over panel
+        Calls the stages of the game: 
+        play, get winner and display the game over panel.
         """
         self.play_game()
         self.get_who_won()
         self.show_gameover()
-    
+
     def show_gameover(self):
         """
-        Shows the "game over" message and asks if the user wants to play again
-        (or see another game, if it was computer vs computer) and if she/he 
+        Shows the "game over" message and, if the user was playing,
+        asks if the user wants to play again and if she/he
         wants to know to strategy of the opponent next time.
+        If the game was computer vs computer, asks if the user wants
+        to see another game.
         """
         print("\n    Game over!\n")
-        if type(self.p1) == HumanPlayer:
-            print('Do you want to play again? (yes or no)')
-            play_again = hinder_invalid_input(SORRY_YES_NO, 'yes no')
-            if play_again == 'yes':
-                print('Do you want to know your opponent\'s strategy? (yes or no)')
-                reveal_mode = hinder_invalid_input(SORRY_YES_NO, 'yes no')
-                if reveal_mode == 'yes':
+        if isinstance(self.p1, HumanPlayer):
+            print("Do you want to play again? (yes or no)")
+            play_again = hinder_invalid_input(SORRY_YES_NO, "yes no")
+            if play_again == "yes":
+                print("Do you want to know your opponent's strategy? (yes or no)")
+                reveal_mode = hinder_invalid_input(SORRY_YES_NO, "yes no")
+                if reveal_mode == "yes":
                     self.reveal_strategy_mode = True
                 else:
                     self.reveal_strategy_mode = False
@@ -96,15 +112,17 @@ class Game:
                 self.run_game()
             else:
                 self.__init__(None, None)
+                self.intro()
         else:
-            print('Do you want to watch another match? (yes or no)')
-            otherMatch = hinder_invalid_input(SORRY_YES_NO, 'yes no')
-            if otherMatch == 'yes':
+            print("Do you want to watch another match? (yes or no)")
+            see_another_match = hinder_invalid_input(SORRY_YES_NO, "yes no")
+            if see_another_match == "yes":
                 self.p1 = random.choice(OPPONENTS)()
                 self.p2 = random.choice(OPPONENTS)()
                 self.run_game()
             else:
                 self.__init__(None, None)
+                self.intro()
 
     def play_game(self):
         """
@@ -112,19 +130,29 @@ class Game:
         """
         print("\n     Game start!\n")
         if self.p1.get_name() == self.p2.get_name():
-             self.p2.name = self.p1.get_name() + ' Bad Twin'
+            self.p2.name = self.p1.get_name() + " Bad Twin"
         for round in range(3):
-            print('\x1b[6;37;41m' + f" X---- ROUND {round+1} ----X "
-                  + '\x1b[0m')
-            if type(self.p1) == HumanPlayer:
-                print('\x1b[33m'+'r'+'\x1b[0m'+'ock, ' + '\x1b[33m' +
-                  'p'+'\x1b[0m' + 'aper or '+'\x1b[33m' +
-                  's'+'\x1b[0m' + 'cissors?')
+            print("\x1b[6;37;41m" + f" X---- ROUND {round+1} ----X " + "\x1b[0m")
+            if isinstance(self.p1, HumanPlayer):
+                print(
+                    "\x1b[33m"
+                    + "r"
+                    + "\x1b[0m"
+                    + "ock, "
+                    + "\x1b[33m"
+                    + "p"
+                    + "\x1b[0m"
+                    + "aper or "
+                    + "\x1b[33m"
+                    + "s"
+                    + "\x1b[0m"
+                    + "cissors?"
+                )
             else:
                 time.sleep(4)
             self.play_round()
 
-    def play_round(self): # TODO: refactor this method, too long
+    def play_round(self):  # TODO: refactor this method, too long
         """
         Core of the game. Takes the moves of the players, apply the rule to 
         get the score, and display the ASCII arts as well as the names and 
@@ -133,8 +161,8 @@ class Game:
         move1 = self.p1.move()
         self.p2.bad_luck(move1)
         move2 = self.p2.move()
-        image1 = ''
-        image2 = ''
+        image1 = ""
+        image2 = ""
         if move1 == MOVES[0]:
             image1 = HANDS[0]
         elif move1 == MOVES[1]:
@@ -149,31 +177,55 @@ class Game:
             image2 = HANDS[2]
         strategy1 = self.p1.tactic()
         strategy2 = self.p2.tactic()
-        if strategy1 == '' and self.reveal_strategy_mode is False:
-            print('\x1b[31m' + f"{self.p1.get_name()}" + '\x1b[0m' +
-                  f": {move1} {image1}" +
-                  '\x1b[34m' + f"{self.p2.get_name()}" + '\x1b[0m' +
-                  f": {move2} {image2}")
-        elif strategy1 == '' and self.reveal_strategy_mode is True:
-            print('\x1b[31m' + f"{self.p1.get_name()}" + '\x1b[0m' +
-                  f" {strategy1}: {move1} {image1}" +
-                  '\x1b[34m' + f"{self.p2.get_name()}" + '\x1b[0m' +
-                  f" {strategy2}: {move2} {image2}")
+        if strategy1 == "" and self.reveal_strategy_mode is False:
+            print(
+                "\x1b[31m"
+                + f"{self.p1.get_name()}"
+                + "\x1b[0m"
+                + f": {move1} {image1}"
+                + "\x1b[34m"
+                + f"{self.p2.get_name()}"
+                + "\x1b[0m"
+                + f": {move2} {image2}"
+            )
+        elif strategy1 == "" and self.reveal_strategy_mode is True:
+            print(
+                "\x1b[31m"
+                + f"{self.p1.get_name()}"
+                + "\x1b[0m"
+                + f" {strategy1}: {move1} {image1}"
+                + "\x1b[34m"
+                + f"{self.p2.get_name()}"
+                + "\x1b[0m"
+                + f" {strategy2}: {move2} {image2}"
+            )
         else:
-            print('\x1b[31m' + f"{self.p1.get_name()}" + '\x1b[0m' +
-                  f" {strategy1}: {move1} {image1}" +
-                  '\x1b[34m' + f"{self.p2.get_name()}" + '\x1b[0m' +
-                  f" {strategy2}: {move2} {image2}")
+            print(
+                "\x1b[31m"
+                + f"{self.p1.get_name()}"
+                + "\x1b[0m"
+                + f" {strategy1}: {move1} {image1}"
+                + "\x1b[34m"
+                + f"{self.p2.get_name()}"
+                + "\x1b[0m"
+                + f" {strategy2}: {move2} {image2}"
+            )
         if beats(move1, move2) is True:
             self.score_p1 += 1
         elif beats(move2, move1) is True:
             self.score_p2 += 1
         else:
             pass
-        print('\n\x1b[31m' + f"{self.p1.get_name()}" + '\x1b[0m' +
-              f": {self.score_p1} point(s) X " +
-              '\x1b[34m' + f"{self.p2.get_name()}" + '\x1b[0m' +
-              f": {self.score_p2} point(s)")
+        print(
+            "\n\x1b[31m"
+            + f"{self.p1.get_name()}"
+            + "\x1b[0m"
+            + f": {self.score_p1} point(s) X "
+            + "\x1b[34m"
+            + f"{self.p2.get_name()}"
+            + "\x1b[0m"
+            + f": {self.score_p2} point(s)"
+        )
         self.p1.learn(move1, move2)
         self.p2.learn(move2, move1)
 
@@ -183,10 +235,10 @@ class Game:
         It also reset the score.
         """
         if self.score_p1 > self.score_p2:
-            print('\n\x1b[31m' + f'{self.p1.get_name()} has won!' + '\x1b[0m')
+            print("\n\x1b[31m" + f"{self.p1.get_name()} has won!" + "\x1b[0m")
         if self.score_p1 < self.score_p2:
-            print('\n\x1b[34m' + f'{self.p2.get_name()} has won!' + '\x1b[0m')
+            print("\n\x1b[34m" + f"{self.p2.get_name()} has won!" + "\x1b[0m")
         if self.score_p1 == self.score_p2:
-            print('\n\x1b[1m' + 'It is a tie!' + '\x1b[0m')
+            print("\n\x1b[1m" + "It is a tie!" + "\x1b[0m")
         self.score_p1 = 0
         self.score_p2 = 0
